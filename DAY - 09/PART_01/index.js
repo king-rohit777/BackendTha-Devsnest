e020397f394a1970d45067978fcc8dc0817dfe15
king-rohit777 <1929235@kiit.ac.in>
2021-09-22 02:31:29 +0530

    ADD DAY 9

---
+++ DAY - 09/PART_01/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+// console.log(__dirname);
+
+app.use('/', (req, res) => {
+    // res.sendFile(path.join(__dirname, "public/test.json"))
+    // res.download(path.join(__dirname, "public/test.json"), "hello.json");
+    // res.render('index', { title: "Express" })
+    res
+        .status(201)
+        .cookie("toke", "test", {
+            expire: new Date(Date.now + 8 * 3600000)
+        })
+        .cookie("hello", "hello")
+        .redirect(301, "/admin")
+})
+
+
+
+//PORT
+app.listen(5000);
